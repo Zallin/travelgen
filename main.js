f07c0d1beddad10c49d10d951dfc33dd8bd8ab4a
Tim Zallin <tim.zallin@gmail.com>
2015-06-19 22:15:02 +0300

    async init added

--- main.js
+++ main.js
@@ -1,38 +1,67 @@
 var express = require('express'),
     swig = require('swig'),
-    tape = require('tape'),
-    MongoClient = require('mongodb').MongoClient,
-    CitiesDAO = require('./db.js');
+    CitiesDAO = require('./db.js'),
+    config = require('./config.json'),
+    async = require('async');
 
-MongoClient.connect('mongodb://localhost/travel', function (err, connection){
+var tape = require('tape');
+
+var Flickr = require('flickrapi'),
+    MongoClient = require('mongodb').MongoClient;
+
+var db, flickr;
+
+async.series([
+  function (cb){
+    MongoClient.connect('mongodb://localhost/travel', function (err, db){
+      if(err) return cb(err, null);
+
+      cb(null, db);
+    });
+  },
+  function (cb){
+    var options = {
+      api_key : config.api_key,
+      secret : config.secret_key
+    }
+
+    Flickr.tokenOnly(options, function (err, flickr){
+      if(err) return cb(err, null);
+
+      cb(null, flickr);
+    });
+  }
+], function (err, res){
   if(err) throw err;
 
-  var app = express();
+  db = new CitiesDAO(res[0]);
+  flickr = res[1];
+});
 
-  var db = new CitiesDAO(connection);
+var app = express();
 
-  app.engine('html', swig.renderFile);
+app.engine('html', swig.renderFile);
 
-  app.use(express.static('public'));
+app.use(express.static('public'));
 
-  app.set('view engine', 'html');
+app.set('view engine', 'html');
 
-  app.get('/', function (req, res, next){
-    db.getCity(function(err, city){
-      if(err) return next(err);
+app.get('/', function (req, res, next){
 
-      res.render('index', {
-        city : city.name
-      });
+  db.getCity(function(err, city){
+    if(err) return next(err);
+
+    res.render('index', {
+      city : city.name
     });
-  });
 
-  app.use(function (err, req, res, next){
-    res.status(500).send();
   });
+});
 
-  app.listen(3000, function (){
-    console.log('listening');
-  });
+app.use(function (err, req, res, next){
+  res.status(500).send();
+});
 
+app.listen(3000, function (){
+  console.log('listening');
 });
